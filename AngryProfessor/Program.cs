707292ca76ab8232779b5707e96b51f691889954
maxx <sh2mg@136@gmail.com>
2022-10-01 20:31:31 +0300

    Picking Numbers

--- AngryProfessor/Program.cs
+++ AngryProfessor/Program.cs
@@ -17,6 +17,17 @@ pairsQty = DivisibleSumPairsSolver.divisibleSumPairs(6, 3, ar);
 Debug.Assert(pairsQty == 5);
 
 
+
+// Picking Numbers
+var arr = new List<int>() { 1, 1, 2, 2, 4, 4, 5, 5, 5 };
+var iRes = PickingNumbersSolver.Run(arr);
+Debug.Assert(iRes == 5);
+
+arr = new List<int>() { 4, 6, 5, 3, 3, 1 };
+iRes = PickingNumbersSolver.Run(arr);
+Debug.Assert(iRes == 3);
+
+
 public class AngryProfessorSolver
 {
     public static string Run(int k, List<int> a)
@@ -28,7 +39,6 @@ public class AngryProfessorSolver
 
 /////////////////////////////
 // Divisible Sum Pairs
-
 public class DivisibleSumPairsSolver
 {
     public static int divisibleSumPairs(int n, int k, List<int> ar)
@@ -42,6 +52,40 @@ public class DivisibleSumPairsSolver
                 if (tmp % k == 0)
                     res++;
             }
+        }
+        return res;
+    }
+}
+
+
+//////////////////////////
+// Picking Numbers
+class PickingNumbersSolver
+{
+    /// <summary>
+    /// Given an array of integers, find the longest subarray 
+    /// where the absolute difference between any two elements 
+    /// is less than or equal to 1.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <returns></returns>
+    public static int Run(List<int> a)
+    {
+        var res = 0;
+        int cnt = 0;
+
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        for (int i = 0; i < a.Count; i++)
+            dict.Add(i, a[i]);
+
+        for (int i = 0; i < a.Count - 1; i++)
+        {
+            cnt = dict.Where(x => x.Key != i && x.Value >= a[i] - 1 && x.Value <= a[i]).Count() + 1;
+            if (cnt > res)
+                res = cnt;
+            cnt = dict.Where(x => x.Key != i && x.Value <= a[i] + 1 && x.Value >= a[i]).Count() + 1;
+            if (cnt > res)
+                res = cnt;
         }
         return res;
     }
