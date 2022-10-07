ee7b9c6aeaea0c08f0b8be85bd00f13a7bb66635
maxx <sh2mg@136@gmail.com>
2022-10-07 16:50:12 +0300

    Sequence Equation Solution

--- CountingValleys/Program.cs
+++ CountingValleys/Program.cs
@@ -1,5 +1,6 @@
 ﻿// https://www.hackerrank.com/challenges/counting-valleys/problem?isFullScreen=true
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 Console.WriteLine("Counting Valleys");
 
@@ -17,6 +18,49 @@ Debug.Assert(result == 1);
 result = CountingValleysSolution.countingValleys(8, "DDUUDDUDUUUD");
 Debug.Assert(result == 2);
 
+/// <summary>
+/// The Hurdle Race
+/// https://www.hackerrank.com/challenges/the-hurdle-race/problem?isFullScreen=true
+/// </summary>
+Console.WriteLine("The Hurdle Race");
+
+result = TheHurdleRaceSolution.hurdleRace(7, new List<int>() { 2, 5, 4, 5, 2 });
+Debug.Assert(result == 0);
+
+result = TheHurdleRaceSolution.hurdleRace(4, new List<int>() { 1, 6, 3, 5, 2 });
+Debug.Assert(result == 2);
+
+
+///
+/// Sequence Equation
+/// https://www.hackerrank.com/challenges/permutation-equation/problem?isFullScreen=true
+/// 
+var list = new List<int>();
+
+list = SequenceEquationSolution.permutationEquation(new List<int>() { 2, 3, 1 });
+var correct = new List<int>() { 2, 3, 1 };
+Debug.Assert(list.Count == correct.Count);
+Debug.Assert(list.Sum() == correct.Sum());
+
+list = SequenceEquationSolution.permutationEquation(new List<int>() { 5, 2, 1, 3, 4 });
+correct = new List<int>() { 4, 2, 5, 1, 3 };
+Debug.Assert(list.Count == correct.Count);
+Debug.Assert(list.Sum() == correct.Sum());
+
+list = SequenceEquationSolution.permutationEquation(new List<int>() { 4, 3, 5, 1, 2 });
+correct = new List<int>() { 1, 3, 5, 4, 2 };
+Debug.Assert(list.Count == correct.Count);
+Debug.Assert(correct.Sum() == list.Sum());
+var dups = list.Intersect(correct).ToList();
+Debug.Assert(dups.Count == list.Count);
+var distinct = list.Except(correct).ToList();
+Debug.Assert(distinct.Count == 0);
+
+
+
+/// <summary>
+///
+/// </summary>
 internal class CountingValleysSolution
 {
     public static int countingValleys(int steps, string path)
@@ -59,6 +103,32 @@ internal class CountingValleysSolution
             }
         }
 
+        return result;
+    }
+}
+
+internal class TheHurdleRaceSolution
+{
+    public static int hurdleRace(int k, List<int> height)
+    {
+        var t = height.Where(x => x > k).ToList();
+        return t.Any() ? t.Max() - k : 0;
+    }
+}
+
+internal class SequenceEquationSolution
+{
+    public static List<int> permutationEquation(List<int> p)
+    {
+        var result = new List<int>();
+        int cnt = 0;
+        var dict = p.ToDictionary(x => cnt++, x => x);
+        foreach (var ii in Enumerable.Range(0, p.Count))
+        {
+            var pi = dict.Where(x => x.Value == ii + 1).First().Key;
+            var pi2 = dict.Where(x => x.Value == pi + 1).First().Key;
+            result.Add(pi2 + 1);
+        }
         return result;
     }
 }
