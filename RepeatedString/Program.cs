c719834f4088ec39c18aa50734acff49bbce8b21
maxx <sh2mg@136@gmail.com>
2022-10-08 12:52:21 +0300

    Repeated String

---
+++ RepeatedString/Program.cs
@@ -0,0 +1,20 @@
+﻿using RepeatedString;
+using System.Diagnostics;
+
+//
+//
+// Repeated String
+// https://www.hackerrank.com/challenges/repeated-string/problem?isFullScreen=true
+Console.WriteLine("Repeated String");
+
+long a_count = RepeatedStringSolution.repeatedString("abcac", 10);
+Debug.Assert(a_count == 4);
+
+a_count = RepeatedStringSolution.repeatedString("aba", 10);
+Debug.Assert(a_count == 7);
+
+Console.WriteLine(Environment.NewLine);
+Console.WriteLine(Environment.NewLine);
+//
+//
+//
---
+++ RepeatedString/RepeatedString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatedString
+{
+    internal class RepeatedStringSolution
+    {
+        public static long repeatedString(string s, long n)
+        {
+            if (s.Length == 0) return 0;
+
+            var k = n / s.Length;
+            var o = n % s.Length;
+
+            var cnt = s.Where(x => x == 'a').Count();
+
+            var extra_cnt = 0;
+
+            if (o > 0)
+            {
+                extra_cnt = s.Substring(0, (int)o).Where(x => x == 'a').Count();
+            }
+
+            return cnt * k + extra_cnt;
+        }
+    }
+}
