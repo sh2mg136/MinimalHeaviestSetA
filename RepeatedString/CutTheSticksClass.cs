7859d0f56112b2d0c0fe3e8c92d3004a15b54284
maxx <sh2mg@136@gmail.com>
2022-10-08 13:15:41 +0300

    Cut the sticks done

---
+++ RepeatedString/CutTheSticksClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatedString
+{
+    internal class CutTheSticksClass
+    {
+        public static List<int> cutTheSticks(List<int> arr)
+        {
+            var result = new List<int>() { arr.Count };
+
+            while (arr.Count > 1)
+            {
+                var min = arr.Min();
+                var sub = arr.Where(x => x != min).ToList();
+                var cnt = 0;
+                arr.Clear();
+                foreach (var item in sub)
+                {
+                    arr.Add(item - min);
+                    cnt++;
+                }
+                if (cnt > 0) result.Add(cnt);
+            }
+
+            return result;
+        }
+    }
+}
--- RepeatedString/Program.cs
+++ RepeatedString/Program.cs
@@ -18,3 +18,21 @@ Console.WriteLine(Environment.NewLine);
 //
 //
 //
+// Cut the sticks
+// https://www.hackerrank.com/challenges/cut-the-sticks/problem?isFullScreen=true
+Console.WriteLine("Cut the sticks");
+var input = new List<int>() { 1, 2, 3, 4, 3, 3, 2, 1 };
+var output = new List<int>();
+var correct = new List<int>() { 8, 6, 4, 1 };
+output = CutTheSticksClass.cutTheSticks(input);
+Debug.Assert(output.Sum() == correct.Sum());
+
+input = new List<int>() { 5, 4, 4, 2, 2, 8 };
+correct = new List<int>() { 6, 4, 2, 1 };
+output = CutTheSticksClass.cutTheSticks(input);
+Debug.Assert(output.Sum() == correct.Sum());
+
+input = new List<int>() { 8, 8, 14, 10, 3, 5, 14, 12 };
+correct = new List<int>() { 8, 7, 6, 4, 3, 2 };
+output = CutTheSticksClass.cutTheSticks(input);
+Debug.Assert(output.Sum() == correct.Sum());
