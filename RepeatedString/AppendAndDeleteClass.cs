ccff65284012b430568f8e4d25ffd338255b7bfd
maxx <sh2mg@136@gmail.com>
2022-10-09 13:05:30 +0300

    Append and Delete

---
+++ RepeatedString/AppendAndDeleteClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatedString
+{
+    internal class AppendAndDeleteClass
+    {
+        const string YES = "Yes";
+        const string NO = "No";
+
+        public static string appendAndDelete(string s, string t, int k)
+        {
+            var first = s.Select(x => (int)x - 97).ToArray();
+            var second = t.Select(x => (int)x - 97).ToArray();
+
+            var minLen = Math.Min(first.Length, second.Length);
+            var fullLen = first.Length + second.Length;
+            var cnt = 0;
+
+            for (int i = 0; i < minLen; i++)
+            {
+                if (first[i] == second[i])
+                    cnt++;
+                else break;
+            }
+
+            var ost1 = first.Length - cnt;
+            var ost2 = second.Length - cnt;
+            var diff = ost1 + ost2;
+
+            /*
+            if (diff == 0)
+            {
+                if (first.Length > k) return YES;
+                if (first.Length + second.Length < k) return YES;
+                return NO;
+            }
+
+            if (diff == k && diff % 2 == k % 2)
+                return YES;
+            */
+
+            if (diff > k) return NO;
+            else if (diff == k) return YES;
+            else if (fullLen - k <= 0) return YES;
+            else if (Math.Abs(fullLen - k) % 2 == 0) return YES;
+            return NO;
+        }
+    }
+}
--- RepeatedString/Program.cs
+++ RepeatedString/Program.cs
@@ -36,3 +36,62 @@ input = new List<int>() { 8, 8, 14, 10, 3, 5, 14, 12 };
 correct = new List<int>() { 8, 7, 6, 4, 3, 2 };
 output = CutTheSticksClass.cutTheSticks(input);
 Debug.Assert(output.Sum() == correct.Sum());
+
+
+//
+//
+//
+// Append and Delete
+// https://www.hackerrank.com/challenges/append-and-delete/problem?isFullScreen=true
+Console.WriteLine("Append and Delete");
+string str;
+str = AppendAndDeleteClass.appendAndDelete("abc", "def", 6);
+Debug.Assert(str == "Yes");
+
+str = AppendAndDeleteClass.appendAndDelete("hackerhappy", "hackerrank", 9);
+Debug.Assert(str == "Yes");
+
+str = AppendAndDeleteClass.appendAndDelete("ashley", "ash", 2);
+Debug.Assert(str == "No");
+
+str = AppendAndDeleteClass.appendAndDelete("aba", "aba", 7);
+Debug.Assert(str == "Yes");
+
+str = AppendAndDeleteClass.appendAndDelete("y", "yu", 2);
+Debug.Assert(str == "No");
+
+str = AppendAndDeleteClass.appendAndDelete("yy", "yyu", 4);
+Debug.Assert(str == "No");
+
+str = AppendAndDeleteClass.appendAndDelete(
+    "asdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcv",
+    "asdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcv", 
+    4);
+Debug.Assert(str == "Yes");
+
+str = AppendAndDeleteClass.appendAndDelete("abcd", "abcdert", 10);
+Debug.Assert(str == "No");
+
+str = AppendAndDeleteClass.appendAndDelete(
+    "asdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcv",
+    "bsdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcv",
+    4);
+Debug.Assert(str == "No");
+
+str = AppendAndDeleteClass.appendAndDelete(
+    "asdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcv",
+    "bsdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcvasdfqwertyuighjkzxcv",
+    100);
+Debug.Assert(str == "No");
+
+str = AppendAndDeleteClass.appendAndDelete("aaaaaaaaaa", "aaaaa", 7);
+Debug.Assert(str == "Yes");
+
+str = AppendAndDeleteClass.appendAndDelete("aaa", "aaaaa", 2);
+Debug.Assert(str == "Yes");
+
+str = AppendAndDeleteClass.appendAndDelete("aaa", "aaaaa", 3);
+Debug.Assert(str == "No");
+
+str = AppendAndDeleteClass.appendAndDelete("aaa", "aaaaa", 1);
+Debug.Assert(str == "No");
